599d912039123dbd1400dede517debffbfc4cc9a
Marshall Tuinier <mtuinier@gmail.com>
2017-09-20 13:59:20 -0700

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import IntroContent from './Components/IntroContent'
-import VideoContent from './Components/VideoContent'
-import Nav from './Components/Nav'
-import { fetchGameList, fetchGameRun, fetchGameInfo } from './Utils/api'
-import ReactLoading from 'react-loading'
+import IntroContent from "./Components/IntroContent";
+import VideoContent from "./Components/VideoContent";
+import Nav from "./Components/Nav";
+import { fetchGameList, fetchGameRun, fetchGameInfo } from "./Utils/api";
+import ReactLoading from "react-loading";
 
 class App extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       gameList: [],
@@ -18,16 +18,16 @@ class App extends Component {
       hasInfo: false,
       buttonsDisabled: true,
       navArray: [],
-      showLoading: false,
-    }
+      showLoading: false
+    };
   }
 
   //This method is called each time a new run is selected.  The old navObj is passed to the navArray in order to be displayed in the sidebar
-  handleNavArray = (navObj) => {
+  handleNavArray = navObj => {
     let newArray = this.state.navArray;
     newArray.push(navObj);
-    this.setState({navArray: newArray})
-  }
+    this.setState({ navArray: newArray });
+  };
 
   //Sets the App for a new game. The potentialId is an optional parameter used if a given run is to be rewatched
   handleNewGame = (...potentialId) => {
@@ -38,72 +38,73 @@ class App extends Component {
       hasRun: false,
       hasInfo: false,
       gameId: null,
-      showLoading: true,
-    })
+      showLoading: true
+    });
     //Assign gameId to a random game's id, from the data pulled from the API
-    let gameId = this.state.gameList[Math.floor(Math.random() * this.state.gameList.length)].id;
+    let gameId = this.state.gameList[
+      Math.floor(Math.random() * this.state.gameList.length)
+    ].id;
 
     //If potentialId was passed to the function, assign our gameId to that param
-    console.log(potentialId)
-    if (typeof potentialId[0] === 'string') {
+    if (typeof potentialId[0] === "string") {
       gameId = potentialId[0];
     }
 
     //Fetch the data of the randomly selected game
-    fetchGameInfo(gameId)
-      .then(data => {
-        this.setState({
-          currentGameInfo: data,
-          hasInfo: true,
-          gameId: gameId,
-        })
-      })
+    fetchGameInfo(gameId).then(data => {
+      this.setState({
+        currentGameInfo: data,
+        hasInfo: true,
+        gameId: gameId
+      });
+    });
 
     //Fetch the data for the actual speedrun information of the selected game
-    fetchGameRun(gameId)
-      .then(data => {
-        this.setState({currentGameRun: data})
+    fetchGameRun(gameId).then(data => {
+      this.setState({ currentGameRun: data });
 
-        //Parameter check to see if all the neccessary information about the run exists.  If it does not, the function re-calls iteself to select another game
-        if (data.length > 0 && data[0].runs.length > 0 && data[0].runs[0].run.videos && data[0].runs[0].run.videos.links) {
-          this.setState({
-            mainSeen: true,
-            hasRun: true,
-          })
-        } else {
-          this.handleNewGame();
-        }
-      })
-  }
+      //Parameter check to see if all the neccessary information about the run exists.  If it does not, the function re-calls iteself to select another game
+      if (
+        data.length > 0 &&
+        data[0].runs.length > 0 &&
+        data[0].runs[0].run.videos &&
+        data[0].runs[0].run.videos.links
+      ) {
+        this.setState({
+          mainSeen: true,
+          hasRun: true
+        });
+      } else {
+        this.handleNewGame();
+      }
+    });
+  };
 
   componentDidMount = () => {
     //Here we define our listFetcher helper function, using a fetchGameList helper from the Utils folder.
     //This will populate our gameList state with all the potential games to choose from.
-    const listFetcher = (url) => {
-      fetchGameList(url)
-      .then(data => {
-        this.setState(prevState => (
-          {
-            gameList: prevState.gameList.concat(data.data),
-            buttonsDisabled: false
-          }
-        ));
+    const listFetcher = url => {
+      fetchGameList(url).then(data => {
+        this.setState(prevState => ({
+          gameList: prevState.gameList.concat(data.data),
+          buttonsDisabled: false
+        }));
         //pagination to continue fetching
         data.pagination.links.forEach(obj => {
-          if (obj.rel === 'next') {
+          if (obj.rel === "next") {
             listFetcher(obj.uri);
           }
-        })
-      })
-    }
+        });
+      });
+    };
 
     //Actual call to the API
-    listFetcher('http://www.speedrun.com/api/v1/games?_bulk=yes&max=1000')
-  }
+    listFetcher("http://www.speedrun.com/api/v1/games?_bulk=yes&max=1000");
+  };
 
   render() {
     return (
-      <div className='home-container'>
+      <div className="home-container">
         <Nav
           handleNewGame={this.handleNewGame}
           buttonsDisabled={this.state.buttonsDisabled}
@@ -111,32 +112,37 @@ class App extends Component {
         />
 
         {/*On first page load, we display our intro content*/}
-                {!this.state.mainSeen &&
+        {!this.state.mainSeen &&
           <IntroContent
             handleNewGame={this.handleNewGame}
             buttonsDisabled={this.state.buttonsDisabled}
-          />
-        }
+          />}
 
         {/*If the introContent has been bypassed, render a loading component if current data is still being fetched*/}
-        {this.state.mainSeen && !(this.state.hasRun && this.state.hasInfo) &&
-          <div className='loading'>
-            <ReactLoading type='spinningBubbles' color='#5ab9ea' height='200px' width='200px'/>
-          </div>
-        }
+        {this.state.mainSeen &&
+          !(this.state.hasRun && this.state.hasInfo) &&
+          <div className="loading">
+            <ReactLoading
+              type="spinningBubbles"
+              color="#5ab9ea"
+              height="200px"
+              width="200px"
+            />
+          </div>}
 
         {/*If everything has loaded, we display the video content*/}
-        {this.state.mainSeen && this.state.hasRun && this.state.hasInfo &&
+        {this.state.mainSeen &&
+          this.state.hasRun &&
+          this.state.hasInfo &&
           <VideoContent
             handleNavArray={this.handleNavArray}
             url={this.state.currentGameRun[0].runs[0].run.videos.links[0].uri}
             gameInfo={this.state.currentGameInfo}
             runInfo={this.state.currentGameRun}
             gameId={this.state.gameId}
-          />
-        }
+          />}
       </div>
-    )
+    );
   }
 }
 
--- src/Components/RunData.js
+++ src/Components/RunData.js
@@ -1,64 +1,64 @@
-import React from 'react'
-import timeChanger from '../Utils/timeChanger'
-import { fetchRunnerData, fetchCategory } from '../Utils/api'
-import RunInfo from './RunInfo'
+import React from "react";
+import timeChanger from "../Utils/timeChanger";
+import { fetchRunnerData, fetchCategory } from "../Utils/api";
+import RunInfo from "./RunInfo";
 class RunData extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       runnerData: null,
       categoryData: null,
-      navObj: {},
-    }
+      navObj: {}
+    };
   }
   componentDidMount() {
-    
     //Here we fetch data for the runner/streamer, as this is contained in a seperate endpoint than the game information
-    fetchRunnerData(this.props.runInfo[0].runs[0].run.players[0].uri.slice(7))
-      .then(data => {
-        let newNavObj = this.state.navObj;
-        newNavObj.gameName = this.props.gameInfo.names.twitch;
+    fetchRunnerData(
+      //grab the url from this particular run
+      this.props.runInfo[0].runs[0].run.players[0].uri.slice(8)
+    ).then(data => {
+      let newNavObj = this.state.navObj;
+      newNavObj.gameName = this.props.gameInfo.names.twitch;
 
-        //Property check to ensure the runner has a name
-        if (data.hasOwnProperty('names')) {
-          newNavObj.runnerName = data.names.international;
-        }
-        else{
-          newNavObj.runnerName = data.name;
-        }
+      //Property check to ensure the runner has a name
+      if (data.hasOwnProperty("names")) {
+        newNavObj.runnerName = data.names.international;
+      } else {
+        newNavObj.runnerName = data.name;
+      }
 
-        //Convert the run time in seconds to HH:MM:SS
-        newNavObj.time = timeChanger(this.props.runInfo[0].runs[0].run.times.primary_t);
-        newNavObj.gameId = this.props.gameId;
+      //Convert the run time in seconds to HH:MM:SS
+      newNavObj.time = timeChanger(
+        this.props.runInfo[0].runs[0].run.times.primary_t
+      );
+      newNavObj.gameId = this.props.gameId;
 
-        //Link to the runners speedrun database
-        newNavObj.siteLink = data.weblink;
+      //Link to the runners speedrun database
+      newNavObj.siteLink = data.weblink;
 
-        //Link to the runners Twitch account
-        if (!!data.twitch) {
-          newNavObj.twitchLink = data.twitch.uri;
-        }
-        else {
-          newNavObj.twtichLink = '#';
-        }
-        this.setState({
-          runnerData: data,
-          navObj: newNavObj,
-        })
-      })
+      //Link to the runners Twitch account
+      if (!!data.twitch) {
+        newNavObj.twitchLink = data.twitch.uri;
+      } else {
+        newNavObj.twtichLink = "#";
+      }
+      this.setState({
+        runnerData: data,
+        navObj: newNavObj
+      });
+    });
 
     //Fetch all the information for the run category (Any%, 100%, etc)
     //The run category information is given to us in the shape of an ID and
     //We need to make a seperate API call to get the actual string name
-    fetchCategory(this.props.runInfo[0].category)
-      .then(data => {
-        let newNavObj = this.state.navObj;
-        newNavObj.categoryName = data.name;
-        this.setState({
-          categoryData: data,
-          navObj: newNavObj,
-        })
-      })
+    fetchCategory(this.props.runInfo[0].category).then(data => {
+      let newNavObj = this.state.navObj;
+      newNavObj.categoryName = data.name;
+      this.setState({
+        categoryData: data,
+        navObj: newNavObj
+      });
+    });
   }
 
   //Once the run is finished and the user has chosen to view another
@@ -68,15 +68,15 @@ class RunData extends React.Component {
   }
 
   render() {
-    return(
-      <div className='run-data-container'>
-        {this.state.runnerData && this.state.categoryData &&
+    return (
+      <div className="run-data-container">
+        {this.state.runnerData &&
+          this.state.categoryData &&
           <RunInfo>
             {this.state.navObj}
-          </RunInfo>
-        }
+          </RunInfo>}
       </div>
-    )
+    );
   }
 }
 
