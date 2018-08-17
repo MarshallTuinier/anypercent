c31e12b010dd04f945b935d82e23b2b7cec6288d
Marshall Tuinier <mtuinier@gmail.com>
2018-08-17 11:55:59 -0700

    comment changes

--- src/Components/VideoContent.js
+++ src/Components/VideoContent.js
@@ -1,7 +1,7 @@
-import React from "react";
-import parseUrl from "parse-url";
-import RunData from "./RunData";
-import placeholderImage from "../Assets/placeholderImage.png";
+import React from 'react';
+import parseUrl from 'parse-url';
+import RunData from './RunData';
+import placeholderImage from '../Assets/placeholderImage.png';
 const VideoContent = props => {
   //The VideoConent object gets the URL from out data, and displays the video
   //However, our data from the API comes from multiple video sources (twitch/Youtube)
@@ -10,30 +10,30 @@ const VideoContent = props => {
   let id;
   let url;
   const urlObj = parseUrl(props.url);
-  if (urlObj.resource === "www.twitch.tv") {
-    if (urlObj.pathname.indexOf("/videos/") > -1) {
-      id = urlObj.pathname.slice(urlObj.pathname.indexOf("/videos/") + 8);
-    } else if (urlObj.pathname.indexOf("/v/") > -1) {
-      id = urlObj.pathname.slice(urlObj.pathname.indexOf("/v/") + 3);
-    } else if (urlObj.pathname.indexOf("/c/") > -1) {
-      id = urlObj.pathname.slice(urlObj.pathname.indexOf("/c/") + 3);
+  if (urlObj.resource === 'www.twitch.tv') {
+    if (urlObj.pathname.indexOf('/videos/') > -1) {
+      id = urlObj.pathname.slice(urlObj.pathname.indexOf('/videos/') + 8);
+    } else if (urlObj.pathname.indexOf('/v/') > -1) {
+      id = urlObj.pathname.slice(urlObj.pathname.indexOf('/v/') + 3);
+    } else if (urlObj.pathname.indexOf('/c/') > -1) {
+      id = urlObj.pathname.slice(urlObj.pathname.indexOf('/c/') + 3);
     }
     //Below we set our URL to the actual embedded link to be played in the iframe based on the video source
     url = `https://player.twitch.tv/?video=v${id}&autoplay=true`;
-  } else if (urlObj.resource === "www.youtube.com") {
+  } else if (urlObj.resource === 'www.youtube.com') {
     id = urlObj.search.slice(2);
     url = `https://www.youtube.com/embed/${id}?autoplay=1`;
-  } else if (urlObj.resource === "youtu.be") {
+  } else if (urlObj.resource === 'youtu.be') {
     id = urlObj.pathname.slice(1);
     url = `https://www.youtube.com/embed/${id}?autoplay=1`;
   } else {
-    url = "";
+    url = '';
   }
   return (
     <div className="video-content">
       <div className="iframe-container">
         {/*Below is a bit of a 'hack.' We get a placeholder image to be scaled along the inside of the iframe container based on the window size.
-           This allows us to essentially attach the video to that image, making the iframe video responsive without having
+           This allows us to essentially attach the video to that image, making the iframe video responsive with a fixed aspect ratio without having
            to define set heights and widths to the video component
         */}
         <img
